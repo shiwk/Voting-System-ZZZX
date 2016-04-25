243ed21a342fd72c1f696043384e8acd5d0cc6ca
Teven Feng <fengdingwen@outlook.com>
2016-04-25 14:32:11 +0800

    test

--- resources/views/wechat.blade.php
+++ resources/views/wechat.blade.php
@@ -13,9 +13,9 @@ use LaneWeChat\Core\Wechat;
  * @Blog: Http://www.lanecn.com
  */
 //引入配置文件
-include_once app_path().'/Libs/LaneWechat/config.php';
+include_once app_path().'/Libs/LaneWeChat/config.php';
 //引入自动载入函数
-include_once app_path().'/Libs/LaneWechat/autoloader.php';
+include_once app_path().'/Libs/LaneWeChat/autoloader.php';
 //调用自动载入函数
 AutoLoader::register();
 //初始化微信类
