03bced30341fb09620c7ac3adcd5101d0bc23782
Teven Feng <fengdingwen@outlook.com>
2016-04-26 14:15:09 +0800

    reconfig database

--- database/migrations/2016_04_19_112853_create_table_activities.php
+++ database/migrations/2016_04_19_112853_create_table_activities.php
@@ -13,16 +13,18 @@ class CreateTableActivities extends Migration
     public function up()
     {
         Schema::create('activities', function (Blueprint $table) {
-            $table->bigIncrements('act_id');
+            $table->bigIncrements('act_id');            
             $table->string('act_name', 255);
             $table->binary('act_discription')->nullable();
             $table->binary('act_rule')->nullable();
             $table->binary('act_prizes')->nullable();
+            $table->int('act_candidate_num');
             $table->integer('act_vote_upto');
             $table->integer('act_vote_atleast');
             $table->dateTime('act_start_time');
             $table->dateTime('act_stop_time');
             $table->integer('act_candidates_num');
+            $table->bigint('act_view_statistics')->nullable();
         });
     }
 
